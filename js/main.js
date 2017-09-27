0edd0e6411c837fd33cd9c7234d8ff6347b4f95e
Irina <loyrina@gmail.com>
2017-09-27 23:25:01 +0400

    fixed consts and functions

--- js/main.js
+++ js/main.js
@@ -1,29 +1,32 @@
+const Key = {
+  ARROW_LEFT_KEY_CODE: 37,
+  ARROW_RIGHT_KEY_CODE: 39
+};
 const screens = document.querySelectorAll(`template`);
+const firstScreen = 0;
 const lastScreen = screens.length - 1;
 const mainScreen = document.querySelector(`.central`);
 let currentScreen = 0;
-const AROOW_LEFT_KEY_CODE = 37;
-const ARROW_RIGHT_KEY_CODE = 39;
 
-const showScreen = function (number) {
+const showScreen = (number) => {
   mainScreen.innerHTML = screens[number].innerHTML;
 };
 
 showScreen(currentScreen);
 
-const changeScreen = function (evt) {
-  if ((evt.altKey) && (evt.keyCode === AROOW_LEFT_KEY_CODE)) {
-    if (currentScreen === 0) {
+const changeScreen = (evt) => {
+  if ((evt.altKey) && (evt.keyCode === Key.ARROW_LEFT_KEY_CODE)) {
+    if (currentScreen === firstScreen) {
       currentScreen = lastScreen;
       showScreen(currentScreen);
     } else {
       showScreen(--currentScreen);
     }
-  } else if ((evt.altKey) && (evt.keyCode === ARROW_RIGHT_KEY_CODE)) {
+  } else if ((evt.altKey) && (evt.keyCode === Key.ARROW_RIGHT_KEY_CODE)) {
     if (currentScreen < lastScreen) {
       showScreen(++currentScreen);
     } else {
-      currentScreen = 0;
+      currentScreen = firstScreen;
       showScreen(currentScreen);
     }
   }
