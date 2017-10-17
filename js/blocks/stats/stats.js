7451855eb5779edaaa65ab98c8d792c2ecc3022b
Irina <loyrina@gmail.com>
2017-10-17 20:38:48 +0400

    game history added

--- js/blocks/stats/stats.js
+++ js/blocks/stats/stats.js
@@ -33,88 +33,35 @@ const getResultAnswers = (data) => {
   };
 };
 
-const oldGames = String.raw`<table class="result__table">
-      <tr>
-        <td class="result__number">2.</td>
-        <td>
-          <ul class="stats">
-            <li class="stats__result stats__result--wrong"></li>
-            <li class="stats__result stats__result--slow"></li>
-            <li class="stats__result stats__result--fast"></li>
-            <li class="stats__result stats__result--correct"></li>
-            <li class="stats__result stats__result--wrong"></li>
-            <li class="stats__result stats__result--unknown"></li>
-            <li class="stats__result stats__result--slow"></li>
-            <li class="stats__result stats__result--wrong"></li>
-            <li class="stats__result stats__result--fast"></li>
-            <li class="stats__result stats__result--wrong"></li>
-          </ul>
-        </td>
-        <td class="result__total"></td>
-        <td class="result__total  result__total--final">fail</td>
-      </tr>
-    </table>
-    <table class="result__table">
-      <tr>
-        <td class="result__number">3.</td>
-        <td colspan="2">
-          <ul class="stats">
-            <li class="stats__result stats__result--wrong"></li>
-            <li class="stats__result stats__result--slow"></li>
-            <li class="stats__result stats__result--fast"></li>
-            <li class="stats__result stats__result--correct"></li>
-            <li class="stats__result stats__result--wrong"></li>
-            <li class="stats__result stats__result--unknown"></li>
-            <li class="stats__result stats__result--slow"></li>
-            <li class="stats__result stats__result--unknown"></li>
-            <li class="stats__result stats__result--fast"></li>
-            <li class="stats__result stats__result--unknown"></li>
-          </ul>
-        </td>
-        <td class="result__points">×&nbsp;100</td>
-        <td class="result__total">900</td>
-      </tr>
-      <tr>
-        <td></td>
-        <td class="result__extra">Бонус за жизни:</td>
-        <td class="result__extra">2&nbsp;<span class="stats__result stats__result--alive"></span></td>
-        <td class="result__points">×&nbsp;50</td>
-        <td class="result__total">100</td>
-      </tr>
-      <tr>
-        <td colspan="5" class="result__total  result__total--final">950</td>
-      </tr>
-    </table>`;
+const correctPoints = (data) => {
+  return data.correct * 100;
+};
+
+const fastPoints = (data) => {
+  return data.fast * 50;
+};
+
+const livePoints = (data) => {
+  return data.lives * 50;
+};
+
+const slowPoints = (data) => {
+  return data.slow * -50;
+};
+
+const totalPoints = (userResults, finalResults) => {
+  return correctPoints(finalResults) + fastPoints(finalResults) + livePoints(userResults) + slowPoints(finalResults);
+};
 
 const getGameResults = (data) => {
   const gameResults = getResultAnswers(data);
 
   const resultTitle = () => {
-    if (gameResults.wrong > 3) {
+    if (gameResults.wrong > 2) {
       return `FAIL`;
     } else {
       return `Победа!`;
     }
-  };
-
-  const correctPoints = () => {
-    return gameResults.correct * 100;
-  };
-
-  const fastPoints = () => {
-    return gameResults.fast * 50;
-  };
-
-  const livePoints = () => {
-    return data.lives * 50;
-  };
-
-  const slowPoints = () => {
-    return gameResults.slow * -50;
-  };
-
-  const totalPoints = () => {
-    return correctPoints() + fastPoints() + livePoints() + slowPoints();
   };
 
   const currentStats = String.raw`<div class="result">
@@ -128,31 +75,31 @@ const getGameResults = (data) => {
           </ul>
         </td>
         <td class="result__points">×&nbsp;100</td>
-        <td class="result__total">${correctPoints()}</td>
+        <td class="result__total">${correctPoints(gameResults)}</td>
       </tr>
       <tr>
         <td></td>
         <td class="result__extra">Бонус за скорость:</td>
         <td class="result__extra">${gameResults.fast}&nbsp;<span class="stats__result stats__result--fast"></span></td>
         <td class="result__points">×&nbsp;50</td>
-        <td class="result__total">${fastPoints()}</td>
+        <td class="result__total">${fastPoints(gameResults)}</td>
       </tr>
       <tr>
         <td></td>
         <td class="result__extra">Бонус за жизни:</td>
         <td class="result__extra">${data.lives}&nbsp;<span class="stats__result stats__result--alive"></span></td>
         <td class="result__points">×&nbsp;50</td>
-        <td class="result__total">${livePoints()}</td>
+        <td class="result__total">${livePoints(data)}</td>
       </tr>
       <tr>
         <td></td>
         <td class="result__extra">Штраф за медлительность:</td>
         <td class="result__extra">${gameResults.slow}&nbsp;<span class="stats__result stats__result--slow"></span></td>
         <td class="result__points">×&nbsp;50</td>
-        <td class="result__total">${slowPoints()}</td>
+        <td class="result__total">${slowPoints(gameResults)}</td>
       </tr>
       <tr>
-        <td colspan="5" class="result__total  result__total--final">${totalPoints()}</td>
+        <td colspan="5" class="result__total  result__total--final">${totalPoints(data, gameResults)}</td>
       </tr>
     </table>    
   </div>`;
@@ -160,12 +107,71 @@ const getGameResults = (data) => {
   return currentStats;
 };
 
+let gameHistory = [];
+
+const addToHistory = (data) => {
+  gameHistory.push(data);
+
+  if (gameHistory.length > 2 ) {
+    gameHistory.shift();
+  }
+  return gameHistory;
+};
+
+const getGameHistory = (historyData) => {
+  let oldGames = ``;
+  let historyGameResults = {};
+  for (let i = 0; i < historyData.length; i++) {
+    historyGameResults = getResultAnswers(historyData[i]);
+
+    oldGames = `${oldGames} <table class="result__table">
+      <tr>
+        <td class="result__number">${i + 2}</td>
+        <td colspan="2">
+          <ul class="stats">
+            ${[...historyData[i].results].map((level) => `<li class="stats__result stats__result--${level}"></li>`)}
+          </ul>
+        </td>
+        <td class="result__points">×&nbsp;100</td>
+        <td class="result__total">${correctPoints(historyGameResults)}</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td class="result__extra">Бонус за скорость:</td>
+        <td class="result__extra">${historyGameResults.fast}&nbsp;<span class="stats__result stats__result--fast"></span></td>
+        <td class="result__points">×&nbsp;50</td>
+        <td class="result__total">${fastPoints(historyGameResults)}</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td class="result__extra">Бонус за жизни:</td>
+        <td class="result__extra">${historyData[i].lives}&nbsp;<span class="stats__result stats__result--alive"></span></td>
+        <td class="result__points">×&nbsp;50</td>
+        <td class="result__total">${livePoints(historyData[i])}</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td class="result__extra">Штраф за медлительность:</td>
+        <td class="result__extra">${historyGameResults.slow}&nbsp;<span class="stats__result stats__result--slow"></span></td>
+        <td class="result__points">×&nbsp;50</td>
+        <td class="result__total">${slowPoints(historyGameResults)}</td>
+      </tr>
+      <tr>
+        <td colspan="5" class="result__total  result__total--final">${totalPoints(historyData[i], historyGameResults)}</td>
+      </tr>
+    </table>`;
+  }
+  return oldGames;
+};
+
 export default (userData) => {
   const resultStats = `${headerStr}
   <div class="result">
   ${getGameResults(userData)}
-  ${oldGames}
+  ${getGameHistory(gameHistory)}
   </div>`;
+
+  addToHistory(userData);
 
   const moduleStats = createElement(resultStats);
   const back = moduleStats.querySelector(`.back`);
